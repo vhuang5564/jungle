7dc8a11e34dffda9f225364b16edb09bfd7df0ee
unknown <vhuang5564@gmail.com>
2021-08-21 06:10:59 -0700

    define create signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,6 @@ class UsersController < ApplicationController
   end
   
   def create
-    if params.require(:user).permit(:password) != params.require(:user).permit(:password_confirmation)
-      raise "error"
-    end
-
-
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
@@ -20,7 +15,7 @@ class UsersController < ApplicationController
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
   
 end
