bb4e241c57cbe93c5e2b35f66b1f8fb1fb3ece9c
unknown <vhuang5564@gmail.com>
2021-08-20 21:13:27 -0700

    list category in table

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,8 @@
 class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
 
+  def index 
+    @categories = Category.order(id: :desc).all
+  end
+
 end
