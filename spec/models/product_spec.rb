b1dd93eb09974808edf58dd88d581c49041c105d
unknown <vhuang5564@gmail.com>
2021-08-23 20:11:04 -0700

    add validation for name

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    # validation tests/examples here
+    it 'should have a name if presence is true' do
+      @category = Category.new
+      @products = Product.create(name: "Apple", description: "text", image: "string", price_cents: "10", quantity: 1, category_id: 1)
+      raise @products
+      expect(@product.name).to be_valid
+    end
   end
 end
