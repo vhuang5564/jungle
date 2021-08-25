02df5ca1aedfe62c4ad320a4fe8ef66fdefbce94
unknown <vhuang5564@gmail.com>
2021-08-24 19:04:29 -0700

    fix product_spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,25 +6,25 @@ RSpec.describe Product, type: :model do
     it 'should have a name if presence is true' do
       @category = Category.new
       @products = Product.create(id: 1, name: "Apple", description: "text", image: "string", price_cents: "10", quantity: 1, category_id: 1)
-      expect(@products.name).to be_valid
+      expect(@products.name).to be_present
     end
 
     it 'should have a price if presence is true' do
       @category = Category.new
       @products = Product.create(id: 1, name: "Apple", description: "text", image: "string", price_cents: "10", quantity: 1, category_id: 1)
-      expect(@products.price).to be_valid
+      expect(@products.price).to be_present
     end
 
     it 'should have a quantity if presence is true' do
       @category = Category.new
       @products = Product.create(id: 1, name: "Apple", description: "text", image: "string", price_cents: "10", quantity: 1, category_id: 1)
-      expect(@products.quantity).to be_valid
+      expect(@products.quantity).to be_present
     end
 
     it 'should have a category if presence is true' do
       @category = Category.new
       @products = Product.create(id: 1, name: "Apple", description: "text", image: "string", price_cents: "10", quantity: 1, category_id: 1)
-      expect(@products.category_id).to be_valid
+      expect(@products.category_id).to be_present
     end
 
   end
