8832b77f1150f58dbe87df4fd712f98cae51d2b6
unknown <vhuang5564@gmail.com>
2021-08-24 18:24:31 -0700

    add validations for price, quantity, category

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,11 +2,30 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
+
     it 'should have a name if presence is true' do
       @category = Category.new
-      @products = Product.create(name: "Apple", description: "text", image: "string", price_cents: "10", quantity: 1, category_id: 1)
-      raise @products
-      expect(@product.name).to be_valid
+      @products = Product.create(id: 1, name: "Apple", description: "text", image: "string", price_cents: "10", quantity: 1, category_id: 1)
+      expect(@products.name).to be_valid
     end
+
+    it 'should have a price if presence is true' do
+      @category = Category.new
+      @products = Product.create(id: 1, name: "Apple", description: "text", image: "string", price_cents: "10", quantity: 1, category_id: 1)
+      expect(@products.price).to be_valid
+    end
+
+    it 'should have a quantity if presence is true' do
+      @category = Category.new
+      @products = Product.create(id: 1, name: "Apple", description: "text", image: "string", price_cents: "10", quantity: 1, category_id: 1)
+      expect(@products.quantity).to be_valid
+    end
+
+    it 'should have a category if presence is true' do
+      @category = Category.new
+      @products = Product.create(id: 1, name: "Apple", description: "text", image: "string", price_cents: "10", quantity: 1, category_id: 1)
+      expect(@products.category_id).to be_valid
+    end
+
   end
 end
