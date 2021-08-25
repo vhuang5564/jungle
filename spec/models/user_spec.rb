205dcd81a7da14e2ff3e1111cdfcabcd799df1a1
unknown <vhuang5564@gmail.com>
2021-08-24 19:58:15 -0700

    add username validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+
+    it 'must be created with a user name' do
+      user = User.new(id: 1, name: "Name", email: "email@email.com", password_digest: "password")
+      expect(user.name).to be_present
+    end
+
+  end
 end
