208b24b31ff49ee5053cac925b1e8d20cd362530
unknown <vhuang5564@gmail.com>
2021-08-25 04:19:55 -0700

    add error message if name length is not 6-18 characters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
       redirect_to '/'
     else
       redirect_to '/signup'
+      raise "Name length has to be between 6-18 characters"
     end
   end
 
